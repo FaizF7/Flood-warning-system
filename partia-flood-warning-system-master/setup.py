a3485d15b4f858f44b5888ba648df0e7985b4005
FaizF7 <143663093+FaizF7@users.noreply.github.com>
2023-10-02 22:27:54 +0100

    Delete partia-flood-warning-system-master directory

--- partia-flood-warning-system-master/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-setup(name='floodsystem',
-      version='0.1',
-      description='CUED Part IA flood warning system exercise',
-      packages=['floodsystem'],
-      )
